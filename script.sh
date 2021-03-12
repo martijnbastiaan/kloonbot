6d0dce25e0e5f4ce8ae89725372c752fd469be40
Martijn Bastiaan <martijn@hmbastiaan.nl>
2021-03-12 16:15:19 +0100

    wobbleeee

--- script.sh
+++ script.sh
@@ -2,9 +2,15 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+HEADERS="Accept: application/vnd.github.v3+json"
+
 if [[ $KBOT_AUTHOR_ASSOC =~ ^(OWNER|MEMBER|COLLABORATOR)$ ]]; then
   echo $KBOT_COMMENT
   if [[ $KBOT_COMMENT == "@kloonbot run ci"* ]]; then
     echo "woot"
   fi
 fi
+
+pull_request_json=$(curl -H "${HEADERS}" "${KBOT_PULL_REQUEST_URL}")
+
+echo "$pull_request_json" | jq ".head.repo.fork"
